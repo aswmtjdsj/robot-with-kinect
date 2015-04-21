3ee402897065a2aebdd5af563c43b421d18e0d72
Xiang Yu <bupt.aswmtjdsj@gmail.com>
2015-04-21 11:46:02 -0400

    windows common.h

---
+++ windows/common.h
@@ -0,0 +1,14 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+
+#define ASIO_STANDALONE
+#define _WIN32_WINNT 0x0602
+#include "asio-1.10.6\include\asio.hpp"
+#include <cstdlib>
+#include <deque>
+#include <iostream>
+#include <thread>
+#include <iostream>
+
+#endif
