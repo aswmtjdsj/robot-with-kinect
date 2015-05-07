a92c3fb6242da455f2b07533fcb0a81351dd8f7f
Xiang Yu <bupt.aswmtjdsj@gmail.com>
2015-05-06 21:23:49 -0400

    Update common.h

--- windows/common.h
+++ windows/common.h
@@ -4,11 +4,23 @@
 
 #define ASIO_STANDALONE
 #define _WIN32_WINNT 0x0602
+// #include <Windows.h>
 #include "asio-1.10.6\include\asio.hpp"
 #include <cstdlib>
 #include <deque>
 #include <iostream>
 #include <thread>
 #include <iostream>
+#include <locale>
+#include <codecvt>
+
+template<class Interface>
+inline void SafeRelease( Interface *& pInterfaceToRelease )
+{
+	if( pInterfaceToRelease != NULL ){
+		pInterfaceToRelease->Release();
+		pInterfaceToRelease = NULL;
+	}
+}
 
 #endif
