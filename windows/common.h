882c428ebdd7d8cce2a4663a60d11fbf24c74056
Xiang Yu <bupt.aswmtjdsj@gmail.com>
2015-07-20 17:28:41 -0400

    Update common.h

--- windows/common.h
+++ windows/common.h
@@ -6,21 +6,45 @@
 #define _WIN32_WINNT 0x0602
 // #include <Windows.h>
 #include "asio-1.10.6\include\asio.hpp"
+
 #include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <ctime>
+#include <cstdio>
+#include <iomanip>
+#include <sstream>
+
 #include <deque>
-#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
 #include <thread>
-#include <iostream>
+
 #include <locale>
 #include <codecvt>
+#include <comdef.h>
 
 template<class Interface>
-inline void SafeRelease( Interface *& pInterfaceToRelease )
+inline void SafeRelease(Interface *& pInterfaceToRelease )
 {
-	if( pInterfaceToRelease != NULL ){
+	if( pInterfaceToRelease != nullptr ){
 		pInterfaceToRelease->Release();
-		pInterfaceToRelease = NULL;
+		pInterfaceToRelease = nullptr;
 	}
+}
+
+inline std::string wConvToS(const std::wstring & ws) {
+	std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+	std::string s = converter.to_bytes(ws);
+	return s;
+}
+
+inline std::wstring sConvToW(const std::string & s) {
+	std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+	std::wstring ws = converter.from_bytes(s);
+	return ws;
 }
 
 #endif
